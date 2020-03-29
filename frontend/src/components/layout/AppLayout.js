f8b7adcf779268659bd0458f29d6b7a6f34e373c
Ben Monis <slowmonis@gmail.com>
2020-03-29 13:59:45 -0400

    More minor styling

--- frontend/src/components/layout/AppLayout.js
+++ frontend/src/components/layout/AppLayout.js
@@ -3,7 +3,7 @@ import React from 'react';
 const AppLayout = (props) => {
   return (
     <div style={{ backgroundColor: 'white', display: 'flex', justifyContent: 'center', fontFamily: 'monospace' }}>
-      <div style={{ minWidth: '360px' }}>
+      <div style={{ minWidth: '360px', borderRadius: '8px', border: '1px solid transparent', overflow: 'hidden' }}>
         { props.children }
       </div>
     </div>
--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -4,7 +4,7 @@ import User from '../user/User';
 
 const Navbar = () => {
   return (
-    <div style={{ backgroundColor: 'black', display: 'flex', flex: 'row', color: 'white', padding: '8px', height: '48px', boxSizing: 'border-box' }}>
+    <div style={{ backgroundColor: 'black', display: 'flex', flex: 'row', color: 'white', padding: '8px', height: '48px', boxSizing: 'border-box', borderBottom: '1px solid grey' }}>
       <User />
     </div>
   );
